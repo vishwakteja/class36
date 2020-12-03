81118ec95085a425443c5578719894fd5c88f75e
vishwakteja <74646686+vishwakteja@users.noreply.github.com>
2020-12-03 20:49:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var playerCount=0;
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  game= new Game();
+  game.getState();
+  game.start();
+
+
+
+}
+
+function draw(){
+
+  
+}
+
